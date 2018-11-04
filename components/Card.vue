2e2e33a58b3d3b59670b77c73eb454d5a7db6464
Chris Ha <chunghha@users.noreply.github.com>
2018-11-03 22:02:39 -0500

    refactor: apply hover

--- components/Card.vue
+++ components/Card.vue
@@ -1,24 +1,37 @@
 <template>
   <v-layout row wrap>
     <v-flex xs12 class="flag-flex">
-      <v-card class="flag-card">
-        <v-card-media :src="country.flag" height="200px" width="300px" class="flag">
-        </v-card-media>
-        <v-card-title primary-title class="grow">
-          <div>
-            <div class="headline mb-0">{{ country.name }}</div>
-            <div>{{ country.capital }}</div>
-            <div>{{ !!country.population ? country.population.toLocaleString() : '' }}</div>
-            <div>{{ country.subregion }}</div>
-          </div>
-        </v-card-title>
-        <v-card-actions>
-          <v-layout align-center justify-space-between row>
-            <v-btn flat color="deep-purple">Share</v-btn>
-            <v-btn flat color="deep-purple" @click="dialog = true">Explore</v-btn>
-          </v-layout>
-        </v-card-actions>
-      </v-card>
+      <v-hover>
+        <v-card
+          slot-scope="{ hover }"
+          :class="`elevation-${hover ? 12 : 2}`"
+          class="flag-card">
+          <v-img :src="country.flag" height="200px" width="300px" class="flag">
+            <v-expand-transition>
+              <div
+                v-if="hover"
+                class="d-flex transition-fast-in-fast-out grey lighten-4 v-card--reveal
+                  display-2 indigo--text text-xs-center" style="height: 100%;">
+                {{ country.capital }}
+              </div>
+            </v-expand-transition>
+          </v-img>
+          <v-card-title primary-title class="grow">
+            <div>
+              <div class="headline mb-0">{{ country.name }}</div>
+              <div>{{ country.capital }}</div>
+              <div>{{ !!country.population ? country.population.toLocaleString() : '' }}</div>
+              <div>{{ country.subregion }}</div>
+            </div>
+          </v-card-title>
+          <v-card-actions>
+            <v-layout align-center justify-space-between row>
+              <v-btn flat color="deep-purple">Share</v-btn>
+              <v-btn flat color="deep-purple" @click="dialog = true">Explore</v-btn>
+            </v-layout>
+          </v-card-actions>
+        </v-card>
+      </v-hover>
     </v-flex>
 
     <v-dialog
@@ -58,40 +71,45 @@
   </v-layout>
 </template>
 <script lang="ts">
-import {
-  Component,
-  Prop,
-  Vue
-} from "nuxt-property-decorator"
+import { Component, Prop, Vue } from 'nuxt-property-decorator';
 
 @Component({})
 export default class Card extends Vue {
-  @Prop() country
+  @Prop()
+  country;
   dialog = false;
 }
 </script>
 <style scoped>
 .card {
-  font-family: Lato, "Segoe UI", Tahoma, Geneva, Verdana,
-    sans-serif;
+  font-family: Lato, 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 }
 
 .flag {
-  border: 0.25rem solid #f8f6fb;
-  margin-left: 12px;
+  border: 2px solid #fbfbfb;
+  margin-left: 10px;
 }
 
 .flag-card {
   background: #f4f4f4;
-  border: 0.1rem solid white;
+  border: 1px solid white;
   display: flex;
   flex-direction: column;
-  margin: 0.2rem;
+  margin: 0.3rem;
   min-height: 460px;
   padding-top: 1rem;
 }
 
 .flag-flex {
   width: 320px;
+}
+
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  opacity: .92;
+  position: absolute;
+  width: 100%;
 }
 </style>
